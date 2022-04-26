dddd54994b0d06bb5322d13630362a6765a8c7e0
Gabriel <gabriel.gsm2017@gmail.com>
2022-04-26 17:49:34 -0300

    ediçao

--- src/Components/Converter/index.jsx
+++ src/Components/Converter/index.jsx
@@ -1,13 +1,28 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-// import { mask, unMask } from 'remask';
+import IntlCurrencyInput from "react-intl-currency-input";
+
+const currencyConfig = {
+  locale: "pt-BR",
+  formats: {
+    number: {
+      BRL: {
+        style: "currency",
+        currency: "BRL",
+        minimumFractionDigits: 2,
+        maximumFractionDigits: 2,
+      },
+    },
+  },
+};
 
 function Converter() {
   const [dolar, setDolar] = useState();
-  const [resultDolar, setResultDolar] = useState();
   const [euro, setEuro] = useState();
-  const [resultEuro, setResultEuro] = useState();
+  const [coin, setCoin] = useState('USD');
   const [resultReal, setResultReal] = useState();
+  const [valueCoin, setValueCoin] = useState();
+  const maskCoin = new RegExp("^(([\\d]{1,7})(\\,([\\d]{0,2}))?)$");
 
   useEffect(() => {
     axios.get('https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL').then((response) => {
@@ -17,53 +32,62 @@ function Converter() {
       }
     });
   },[]);
+  
+    const getValueSelect = e => {
+      setCoin(e.target.value);
+    }
 
 
-  const calculateReal = e => {
-    setResultReal(e.target.value);
-    setResultDolar(e.target.value / dolar);
-    setResultEuro(e.target.value / euro);
+  const calculateReal = (_e, value, maskedValue) => {
+    _e.preventDefault();
+    if(maskedValue && coin === 'USD'){
+      setValueCoin(value / parseFloat(dolar));
+    }else{
+      setValueCoin(value / parseFloat(euro));
+    }
+    setResultReal(maskedValue);
   }
 
-  const calculateDolar = e => {
-    setResultReal(e.target.value * dolar);
-    setResultEuro(e.target.value * euro);
-    setResultDolar(e.target.value)
+  const calculateCoin = e => {
+    if(e.target.value && coin === 'USD'){
+      setResultReal(parseFloat(e.target.value) * parseFloat(dolar));
+    }else{
+      setResultReal(parseFloat(e.target.value) * parseFloat(euro));
+    }
+    setValueCoin(parseFloat(e.target.value));
   }
-
-  const calculateEuro = e => {
-    setResultEuro(e.target.value);
-    setResultReal(e.target.value / dolar);
-    setResultDolar(e.target.value * dolar);
-  }
-
+  
   return (
     <section className='converter'>
       <h2>Conversor de Moedas</h2>
-      <div className='coin-container'>
-        <label>US$</label>
-        <input 
-          type='text'
-          onChange={calculateDolar}
-          value={resultDolar}
-        />
-      </div>
 
       <div className='coin-container'>
         <label>R$</label>
-        <input 
-          type='text'
+        {/* <input
+          placeholder='Digite o valor'
           onChange={calculateReal}
-          value={resultReal}
-        />
+          value={resultReal} 
+        /> */}
+         <IntlCurrencyInput 
+          currency="BRL" 
+          config={currencyConfig}
+          onChange={calculateReal} />
       </div>
-
       <div className='coin-container'>
-        <label>Є</label>
-        <input 
-          type='text'
-          onChange={calculateEuro}
-          value={resultEuro}
+        <label>Moeda</label>
+        <select 
+          className='styleSelect'
+          onChange={getValueSelect}>
+          <option value='USD'>Dolar</option>
+          <option value='€'>Euro</option>
+        </select>
+      </div>
+       <div className='coin-container'>
+        <label>{coin === "USD" ? "USD": "€"}</label>
+        <inpIntlCurrencyInputut 
+          placeholder='Digite o valor'
+          onChange={calculateCoin}
+          value={valueCoin}
         />
       </div>
     </section>
